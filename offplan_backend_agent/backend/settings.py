d17fd4818ee8afde4103a447f8a4e0b4c5f7aecf
ekaparna3 <ekaparna3@gmail.com>
2025-07-19 11:00:39 +0530

    updated property_type,property_status,sales_status

--- offplan_backend_agent/backend/settings.py
+++ offplan_backend_agent/backend/settings.py
@@ -124,24 +124,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres.xtzdmalumstpitlmiscl',
-#         'PASSWORD': 'D3l3m0n@2025',
-#         'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
-#         'PORT': '5432',
-        
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.xtzdmalumstpitlmiscl',
+        'PASSWORD': 'D3l3m0n@2025',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '5432',
+        
+    }
+}
 
 # DATABASES = {
 #     'default': {
